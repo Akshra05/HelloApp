c1f05ee68fc0f6f1512f1513759d2765cd26a120
Akshra05 <akshramathur@gmail.com>
2026-04-20 23:28:22 +0530

    UC6

--- App/src/main/java/org/example/HelloApp.java
+++ App/src/main/java/org/example/HelloApp.java
@@ -2,8 +2,14 @@ package org.example;
 
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Enter your name:");
-        String nameInput = sc.nextLine();
-        System.out.println("Hello " + nameInput);
+        System.out.println("How many names?");
+        int count = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 0; i < count; i++) {
+            System.out.println("Enter name:");
+            String name1 = sc.nextLine();
+            System.out.println("Hello " + name1);
+        }
     }
 }
